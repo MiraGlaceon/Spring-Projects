04d9f70c5bd10e66362a5cb0102b5d7d84ba9bf0
Denis <jaromirabramov@gmail.com>
2023-02-18 00:40:14 +0600

    Simple configuration of beans test

---
+++ configuration-test/src/main/java/mira/space/configurationtest/ConfigurationTestApplication.java
@@ -0,0 +1,13 @@
+package mira.space.configurationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationTestApplication.class, args);
+	}
+
+}
---
+++ configuration-test/src/test/java/mira/space/configurationtest/ConfigurationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package mira.space.configurationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
